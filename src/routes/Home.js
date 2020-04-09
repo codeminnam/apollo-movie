edaedc1e8ad37c9548a95f4225438aa37bebda9b
cineminnam <cine.minnam@gmail.com>
2020-04-09 23:52:57 +0900

    #1.1 GET_MOVIES Query
    #1.1 GET_MOVIES Query

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,1 +1,27 @@
-export default () => "Home";
+import React from "react";
+import {gql} from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+import styled from "styled-components";
+
+const GET_MOVIES = gql`
+    {
+        movies {
+            id
+            medium_cover_image
+        }
+    }
+`;
+
+export default () => {
+    const {loading, error, data} = useQuery(GET_MOVIES);
+    console.log(loading, error, data);
+    if(loading){
+        return "loading...";
+    }
+    if(data && data.movies){
+        return data.movies.map(m=>
+          <h1>{m.id}</h1>
+        );
+    }
+    return <h1>Home</h1>;
+};
